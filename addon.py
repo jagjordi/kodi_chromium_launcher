e3f3ddcfabd8bd8d9d1830f32abc1cc750dcaa55
Kulturnilpferd <37019912+Kulturnilpferd@users.noreply.github.com>
2019-11-02 17:40:27 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -3,5 +3,5 @@ import xbmc
 
 xbmc.executebuiltin( "XBMC.Action(Stop)")
 xbmc.executebuiltin('Lirc.Stop')
-subprocess.call(['chromium-browser', '--start-maximized'])
+subprocess.call(['chromium-browser', '--start-maximized', '--force-device-scale-factor=1.8'])
 xbmc.executebuiltin('Lirc.Start')
